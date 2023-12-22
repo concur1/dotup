cf5e10bee7b1b67a1edf1e0ed65064333fde15cb
Concur1 <ahcyaskx4@relay.firefox.com>
2023-12-22 10:38:48 +0000

    Refactored file read/write to functions.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::fs::File;
 use std::io::Write;
 use std::io::Read;
 
+use std::collections::HashMap;
 #[derive(Debug, Serialize, Deserialize)]
 struct T {
     system_file_path: String,
@@ -24,27 +25,33 @@ struct FileData {
     paths: Vec<std::option::Option<std::path::PathBuf>>,
 }
 
-fn track(args: Cli) {
-    println!("Track command!");
-
+fn get_file_data () -> FileData {
     let mut read_data = String::new();
     let mut read_file = File::open("./data.json").expect("Unable to open file");
     read_file.read_to_string(&mut read_data).expect("Error converting file contents to string."); 
-    let mut read_file_data: FileData = match serde_json::from_str(&read_data) {
+    let read_file_data: FileData = match serde_json::from_str(&read_data) {
         Ok(v) => v,
         Err(_) => FileData { paths : vec![] }
     };
+    read_file_data
+}
 
+fn write_file_data (file_data: FileData) {
+    let json_output_data = serde_json::to_string(&file_data).expect("egewfg"); 
+    let mut write_file = File::create("./data.json").expect("Unable to create file");
+    write_file.write_all(json_output_data.as_bytes()).expect("Unable to write data");
+}
+
+fn track(args: Cli) {
+    println!("Track command!");
+    let mut read_file_data = get_file_data();
     if read_file_data.paths.contains(&args.path) {
         ()
     } else
     {
     read_file_data.paths.push(args.path);
     let file_data = FileData { paths: read_file_data.paths };
-    let json_output_data = serde_json::to_string(&file_data).expect("egewfg"); 
-    let mut write_file = File::create("./data.json").expect("Unable to create file");
-    write_file.write_all(json_output_data.as_bytes()).expect("Unable to write data");
-    println!("{json_output_data:?}")
+    write_file_data(file_data);
     }
     }
 
