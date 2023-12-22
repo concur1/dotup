fbab5e2e4993d94c97f9aef07209ec954f658a2a
Concur1 <ahcyaskx4@relay.firefox.com>
2023-12-22 13:09:06 +0000

    Updated to use abs paths.

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,12 @@ use serde::{Serialize, Deserialize};
 use std::fs::File;
 use std::io::Write;
 use std::io::Read;
-
 use std::collections::HashMap;
+use std::path::PathBuf;
+use std::path::Path;
+use std::fs;
+use std::fs::OpenOptions;
+
 #[derive(Debug, Serialize, Deserialize)]
 struct T {
     system_file_path: String,
@@ -16,43 +20,52 @@ struct Cli {
     /// The pattern to look for
     action: String,
     /// The path to the file to read
-    path: Option<std::path::PathBuf>,
+    path: PathBuf,
 }
 
 #[derive(Serialize, Deserialize)]
 struct FileData {
     /// The path to the file to read
-    paths: Vec<std::option::Option<std::path::PathBuf>>,
+    paths: HashMap<PathBuf, PathBuf>
 }
 
 fn get_file_data () -> FileData {
+    let tracking_data_path = PathBuf::from(r"data.json");
     let mut read_data = String::new();
-    let mut read_file = File::open("./data.json").expect("Unable to open file");
+    let default = FileData { paths : HashMap::new() };
+    if !!!tracking_data_path.exists() {
+        return default
+    }
+    let mut read_file = File::open("data.json").expect("Unable to open file");
     read_file.read_to_string(&mut read_data).expect("Error converting file contents to string."); 
     let read_file_data: FileData = match serde_json::from_str(&read_data) {
         Ok(v) => v,
-        Err(_) => FileData { paths : vec![] }
+        Err(_) => default
     };
     read_file_data
 }
 
 fn write_file_data (file_data: FileData) {
-    let json_output_data = serde_json::to_string(&file_data).expect("egewfg"); 
-    let mut write_file = File::create("./data.json").expect("Unable to create file");
-    write_file.write_all(json_output_data.as_bytes()).expect("Unable to write data");
+    let tracking_data_path = PathBuf::from(r"data.json");
+    let json_output_data = serde_json::to_string(&file_data).expect("Could not convert to json."); 
+    let mut f = File::create(tracking_data_path).expect("Unable to create file");
+    f.write_all(json_output_data.as_bytes()).expect("Unable to write data");
 }
 
 fn track(args: Cli) {
     println!("Track command!");
+    let abs_path = fs::canonicalize(&args.path).expect("Error getting absolute path.");
+    println!("converting to absolute path: {:?}", abs_path);
     let mut read_file_data = get_file_data();
-    if read_file_data.paths.contains(&args.path) {
-        ()
-    } else
-    {
-    read_file_data.paths.push(args.path);
+    if read_file_data.paths.contains_key(&abs_path) {
+        println!("{abs_path:?} is already tracked.");
+        return
+    }
+    let generic_path = abs_path.clone();
+    read_file_data.paths.insert(abs_path, generic_path);
     let file_data = FileData { paths: read_file_data.paths };
     write_file_data(file_data);
-    }
+    println!("File tracked.")
     }
 
 fn main() {
@@ -62,8 +75,6 @@ fn main() {
         "track" => track(args),
         _ => println!("other action:")
     }
-
-    //sync::sync(Path::new("/home/o/projects/file-backup/output/")).expect("Failed to sync:");
 }
 
 
